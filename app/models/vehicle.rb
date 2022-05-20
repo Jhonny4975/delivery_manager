f602ba01fb16880484deed2e4dafe57ef9ccec66
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-20 14:05:50 -0300

    add validations

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class Vehicle < ApplicationRecord
+  validates :license_plate,
+            :brand_name,
+            :model,
+            :capacity, presence: true
+
+  validates :license_plate, uniqueness: true
+
+  validates :license_plate, format: { with: /\A[A-Z]{3}\d[A-Z]\d{2}\z/,
+                                      message: 'deve ter o formato: AAA0A00' }
 end
