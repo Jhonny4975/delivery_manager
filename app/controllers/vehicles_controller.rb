5e41a960bbd28868afbea278840f0ea7f68cd316
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-20 11:33:49 -0300

    Update vehicles_controller.rb

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class VehiclesController < ApplicationController
+  def index
+    @vehicles = Vehicle.all
+
+    flash.now[:notice] = 'Não existem veículos cadastrados.' if @vehicles.empty?
+  end
 end
