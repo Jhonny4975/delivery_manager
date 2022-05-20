cf0a3f27a7312ba493f0f5e3bcbf38c2561f06bb
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-20 15:13:55 -0300

    add show vehicle action

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class VehiclesController < ApplicationController
+  before_action :set_vehicle, only: %i[show]
+
   def index
     @vehicles = Vehicle.all
 
     flash.now[:notice] = 'Não existem veículos cadastrados.' if @vehicles.empty?
+  end
+
+  def show; end
+
+  private
+
+  def set_vehicle
+    @vehicle = Vehicle.find(params[:id])
   end
 end
