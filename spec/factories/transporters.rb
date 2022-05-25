76d1e226d2d53be99ee3a83d03291a7db88e4d05
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-24 23:16:55 -0300

    translate

--- spec/factories/transporters.rb
+++ spec/factories/transporters.rb
@@ -3,9 +3,13 @@
 FactoryBot.define do
   factory :transporter do
     corporate_name { Faker::Company.name }
-    brand_name { Faker::Company.industry }
-    domain { Faker::Internet.domain_name }
+    brand_name { Faker::Company.unique.industry }
+    domain { set_domain }
     registration_number { Faker::Company.brazilian_company_number(formatted: true) }
     full_address { Faker::Address.full_address }
   end
+end
+
+def set_domain
+  "#{Faker::Alphanumeric.alpha(number: 3)}#{Faker::Internet.free_email.match(/@/).post_match}"
 end
