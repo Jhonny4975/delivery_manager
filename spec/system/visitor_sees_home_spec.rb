654a8e37aa63037945196d386e26c7336ada0da9
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-25 14:39:26 -0300

    restrict access tests

--- spec/system/visitor_sees_home_spec.rb
+++ spec/system/visitor_sees_home_spec.rb
@@ -9,4 +9,20 @@ describe 'visitor accesses home screen' do
     expect(page).to have_content 'Gerenciador de Entregas'
     expect(page).to have_link 'Fazer login'
   end
+
+  it 'and access the vehicles screen' do
+    visit root_path
+    visit transporters_path
+
+    expect(page).to have_content 'You are being redirected.'
+    expect(page).to have_link 'redirected'
+  end
+
+  it 'and access the transporters screen' do
+    visit root_path
+    visit vehicles_path
+
+    expect(page).to have_current_path new_user_session_path
+    expect(page).to have_content 'Faça login, primiro!'
+  end
 end
