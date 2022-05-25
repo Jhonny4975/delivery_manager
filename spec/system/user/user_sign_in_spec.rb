1e0e2dd88133dbe81cedc75a8b26a503d3cc4828
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-24 23:13:42 -0300

    create tests

---
+++ spec/system/user/user_sign_in_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'user accesses login screen' do
+  it 'and log in' do
+    create(:transporter)
+    user = create(:user)
+    # binding.pry
+
+    visit root_path
+    click_on 'Fazer login'
+    within('form') do
+      fill_in 'E-mail', with: user.email
+      fill_in 'Senha', with: user.password
+      click_on 'Login'
+    end
+
+    expect(page).to have_content 'Login efetuado com sucesso.'
+    expect(page).to have_button 'Sair'
+    expect(page).to have_content user.email
+    expect(page).to have_content user.branch_office_name
+  end
+
+  it 'and log out' do
+    create(:transporter)
+    user = create(:user)
+    # binding.pry
+
+    visit new_user_session_path
+    within('form') do
+      fill_in 'E-mail', with: user.email
+      fill_in 'Senha', with: user.password
+      click_on 'Login'
+    end
+    click_on 'Sair'
+
+    expect(page).to have_current_path root_path
+    expect(page).to have_content 'Logout efetuado com sucesso.'
+    expect(page).not_to have_content user.email
+  end
+end
---
+++ spec/system/user/user_sign_up_spec.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'user accesses sign up screen' do
+  it 'and sign up' do
+    create(:transporter)
+    attr = build(:user)
+    # binding.pry
+
+    visit root_path
+    click_on 'Fazer login'
+    click_on 'Nova conta'
+    fill_in 'E-mail', with: attr.email
+    fill_in 'Nome da filial', with: attr.branch_office_name
+    fill_in 'Telefone', with: attr.phone_number
+    fill_in 'Senha', with: attr.password
+    fill_in 'Confirme sua senha', with: attr.password
+    click_on 'Criar Usuário'
+
+    expect(page).to have_content 'Bem vindo! Você realizou seu registro com sucesso.'
+    expect(page).to have_button 'Sair'
+    expect(page).to have_content attr.email
+    expect(page).to have_content attr.branch_office_name
+  end
+
+  it 'with null params' do
+    visit root_path
+    click_on 'Fazer login'
+    click_on 'Nova conta'
+    fill_in 'E-mail', with: ''
+    fill_in 'Senha', with: ''
+    fill_in 'Confirme sua senha', with: ''
+    click_on 'Criar Usuário'
+
+    expect(page).to have_content 'Não foi possível salvar usuário: 3 erros.'
+    expect(page).to have_content 'E-mail não pode ficar em branco'
+    expect(page).to have_content 'Senha não pode ficar em branco'
+    expect(page).to have_content 'E-mail não corresponde a nenhuma transportadora cadastrada'
+  end
+
+  it 'with different passwords' do
+    attr = build(:user)
+
+    visit root_path
+    click_on 'Fazer login'
+    click_on 'Nova conta'
+    fill_in 'E-mail', with: attr.email
+    fill_in 'Senha', with: attr.password
+    fill_in 'Confirme sua senha', with: "#{attr.password}8ad4"
+    click_on 'Criar Usuário'
+
+    expect(page).to have_content 'Não foi possível salvar usuário: 3 erros.'
+    expect(page).to have_content 'Confirme sua senha não é igual a Senha'
+    expect(page).to have_content 'E-mail não corresponde a nenhuma transportadora cadastrada'
+  end
+end
