95dbada0e3be2707278923a5d55e0e56b001f310
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-24 23:16:39 -0300

    generate devise user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_19_205305) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_24_082450) do
   create_table "transporters", force: :cascade do |t|
     t.string "corporate_name", null: false
     t.string "brand_name", null: false
@@ -26,6 +26,24 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_205305) do
     t.index ["registration_number"], name: "unique_registration_number", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "branch_office_name"
+    t.string "phone_number"
+    t.integer "transporter_id"
+    t.boolean "admin", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["branch_office_name"], name: "index_users_on_branch_office_name", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["transporter_id"], name: "index_users_on_transporter_id"
+  end
+
   create_table "vehicles", force: :cascade do |t|
     t.string "license_plate", null: false
     t.string "brand_name", null: false
@@ -37,4 +55,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_205305) do
     t.index ["license_plate"], name: "unique_license_plate", unique: true
   end
 
+  add_foreign_key "users", "transporters"
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    email { admin?(admin) }
+    branch_office_name { Faker::Commerce.brand }
+    phone_number { Faker::PhoneNumber.cell_phone }
+    password { Faker::Internet.password }
+    admin { false }
+  end
+end
+
+def admin?(admin)
+  return "#{Faker::Alphanumeric.alpha(number: 6)}@sistemadefrete.com.br" if admin == true
+
+  "#{Faker::Alphanumeric.alpha(number: 6)}@#{set_transporter_domain}"
+end
+
+def set_transporter_domain
+  Transporter.all.each { |transporter| return transporter.domain if transporter.user.empty? }
+
+  nil
+end
