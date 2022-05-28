d4eb0b75844fa354c48dba30b3a694ef2b81628a
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-28 12:33:58 -0300

    Create budget_data_filter_service.rb

---
+++ app/services/budget_data_filter_service.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+class BudgetDataFilterService
+  attr_reader :deadlines, :transporters, :prices
+
+  def initialize(search_params)
+    @size = search_params[:height].to_d * search_params[:length].to_d * search_params[:width].to_d
+    @deadlines = Deadline.where(max_distance: search_params[:distance].., min_distance: ..search_params[:distance])
+    @budgets = Budget.where(
+      max_size: @size.., min_size: ..@size,
+      max_weight: search_params[:weight]..,
+      min_weight: ..search_params[:weight]
+    )
+  end
+
+  def filter
+    @deadlines = reapeated?(@deadlines)
+    @budgets = reapeated?(@budgets)
+
+    @transporters = @budgets.map(&:transporter)
+  end
+
+  def price_calculator(distance)
+    @prices = @budgets.map do |budget|
+      distance.to_d * budget.price
+    end
+  end
+
+  def reapeated?(objects)
+    transporters_ids = []
+
+    objects.each do |object|
+      if transporters_ids.count(object.transporter_id) >= 1
+        objects = objects.select { |d| d != object }
+      else
+        transporters_ids << object.transporter_id
+      end
+    end
+
+    objects
+  end
+
+  def transporter
+    @transporters.select! do |transporter|
+      transporter.deadline.ids.map do |id|
+        @deadlines.ids.include?(id) unless @deadlines.size < 2
+      end
+    end
+  end
+end
