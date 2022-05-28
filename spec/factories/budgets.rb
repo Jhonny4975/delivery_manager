ec660d40aaf1ef763cd27114e3a5998d22025a18
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-28 12:31:35 -0300

    Update budgets.rb

--- spec/factories/budgets.rb
+++ spec/factories/budgets.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :budget do
-    max_size { Faker::Number.decimal(l_digits: 2) }
-    min_size { Faker::Number.decimal(l_digits: 2) }
-    max_weight { Faker::Number.decimal(l_digits: 2) }
-    min_weight { Faker::Number.decimal(l_digits: 2) }
+    max_size { Faker::Number.decimal(l_digits: 2, r_digits: 1) }
+    min_size { Faker::Number.decimal(l_digits: 1, r_digits: 1) }
+    max_weight { Faker::Number.decimal(l_digits: 2, r_digits: 1) }
+    min_weight { Faker::Number.decimal(l_digits: 1, r_digits: 1) }
     price { Faker::Number.decimal(l_digits: 2) }
     transporter
   end
