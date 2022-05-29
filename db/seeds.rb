7f9157be71a3b29f9f2028f8487b26430b000343
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-28 22:42:33 -0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,39 @@
 # frozen_string_literal: true
 
 puts
-puts '-------------------------------------------------'
+puts '--------------------------------------------------'
 puts 'Start "seeds"'
-puts '-------------------------------------------------'
+puts '--------------------------------------------------'
 puts
 
-puts '------------ creating transporters ------------'
+puts '-------------- creating transporters -------------'
 
 FactoryBot.create_list(:transporter, 2)
+visit_transporter = FactoryBot.create(:transporter, domain: 'gmail.com')
+
+puts '----------------- creating users -----------------'
+
+FactoryBot.create_list(:user, 2)
+visit = FactoryBot.create(:user, email: 'visit@gmail.com', password: '123456')
+FactoryBot.create(:user, email: 'visit@sistemadefrete.com.br', password: '123456')
+
+puts '--------------- creating vehicles ----------------'
+
+FactoryBot.create_list(:vehicle, 2)
+FactoryBot.create(:vehicle, user: visit)
+
+puts '---------------- creating budgets ----------------'
+
+FactoryBot.create_list(:budget, 2)
+FactoryBot.create(:budget, transporter: visit_transporter )
+
+puts '--------------- creating deadlines ---------------'
+
+FactoryBot.create_list(:deadline, 2)
+FactoryBot.create(:deadline, transporter: visit_transporter )
 
 puts
-puts '-------------------------------------------------'
+puts '--------------------------------------------------'
 puts 'Finished "seeds"'
-puts '-------------------------------------------------'
+puts '--------------------------------------------------'
 puts
