6c3c3e21b658e68c5e50776e7d2436d64d9da4ae
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-28 22:37:51 -0300

    fix repeated transporters test

--- spec/system/admin/admin_search_budget_spec.rb
+++ spec/system/admin/admin_search_budget_spec.rb
@@ -48,10 +48,12 @@ describe 'admin access the budget search barr' do
   end
 
   it 'and there are no two quotes from the same transporter' do
-    first_transporter = create(:transporter)
-    create(:budget, price: 0.5, min_weight: 0.1, max_weight: 30, min_size: 0.5, max_size: 2, transporter: first_transporter)
+    first_transporter = create(:transporter, min_price: 300)
+    create(:budget, price: 0.5, min_weight: 0.1, max_weight: 30,
+                    min_size: 0.5, max_size: 2, transporter: first_transporter)
     create(:deadline, period: 2, max_distance: 200, min_distance: 1, transporter: first_transporter)
-    create(:budget, price: 0.7, min_weight: 30, max_weight: 50, min_size: 2, max_size: 3, transporter: first_transporter)
+    create(:budget, price: 0.7, min_weight: 30, max_weight: 50, min_size: 2,
+                    max_size: 3, transporter: first_transporter)
     create(:deadline, period: 4, max_distance: 300, min_distance: 200, transporter: first_transporter)
 
     sign_in create(:user, admin: true)
@@ -63,16 +65,30 @@ describe 'admin access the budget search barr' do
     fill_in 'Distância (km):', with: 200
     click_on 'Consultar'
 
-    expect(page).to have_content 'Orçamentos disponíveis:'
-    expect(page).to have_content 'Transportadora'
-    expect(page).to have_content 'Preço'
-    expect(page).to have_content 'Prazo'
     expect(page).to have_content first_transporter.brand_name, maximum: 1
-    expect(page).to have_content number_to_currency(first_transporter.budget.first.price * 200)
+    expect(page).to have_content number_to_currency(first_transporter.min_price)
     expect(page).to have_content "#{first_transporter.deadline.first.period} dia(s)"
-    expect(page).not_to have_content number_to_currency(first_transporter.budget.last.price * 200)
     expect(page).not_to have_content "#{first_transporter.deadline.last.period} dia(s)"
-    expect(page).to have_link 'Voltar'
+  end
+
+  it 'and does not display inactive transporters' do
+    transporter = create(:transporter, min_price: 300, stats: 'inactive')
+    create(:budget, price: 0.5, min_weight: 0.1, max_weight: 30,
+                    min_size: 0.5, max_size: 2, transporter: transporter)
+    create(:deadline, period: 2, max_distance: 200, min_distance: 1, transporter: transporter)
+
+    sign_in create(:user, admin: true)
+    visit user_root_path
+    fill_in 'Altura do produto (m):', with: 1
+    fill_in 'Comprimento do produto (m):', with: 1
+    fill_in 'Largura do produto (m):', with: 2
+    fill_in 'Peso do produto (kg):', with: 30
+    fill_in 'Distância (km):', with: 200
+    click_on 'Consultar'
+
+    expect(page).not_to have_content transporter.brand_name
+    expect(page).not_to have_content number_to_currency(transporter.min_price)
+    expect(page).not_to have_content "#{transporter.deadline.first.period} dia(s)"
   end
 
   it 'and there is no budgets' do
