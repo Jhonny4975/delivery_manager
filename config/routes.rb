eb3c732d6c6f2172417e3cd4154627a8fafbef1b
Jhonny Toledo <jhonnytoledo4975@gmail.com>
2022-05-30 19:17:14 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
-  resources :transporters, only: %i[index show new create edit update] do
-    get 'search', on: :collection
-  end
-
+  resources :transporters, only: %i[index show new create edit update]
   resources :budgets, only: %i[new create]
   resources :deadlines, only: %i[new create]
   resources :vehicles
+
+  resources :service_orders, only: %i[index create] do
+    get 'search', on: :collection
+  end
 
   get '/welcome' => 'welcome#index', as: :user_root
 end
